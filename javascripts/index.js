a4bf342b07b95deb330b4ed3c27eddd344e4122b
ZoM <paninee@khantidhara.com>
2016-09-05 22:36:28 -0700

    Added membership text

--- javascripts/index.js
+++ javascripts/index.js
@@ -21,4 +21,24 @@ $(function() {
       $('.navbar-inverse').css('background-color', 'rgba(0,0,0,0.0)');
     }
   });
+
+  // Add smooth scrolling to all links inside a navbar
+  $('.discard-navbar a').on('click', function(event){
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash (#)
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, 'easeInOutExpo', function () {
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }
+  });
+
 });
